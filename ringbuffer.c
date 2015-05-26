171b6f73dd7ff7a2e5b8aa875daca708fc6b79f2
Daniel Jedrychowski <daniel.jedrychowski@ecoult.com>
2015-05-26 17:49:15 +1000

    blah

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,17 +1,82 @@
 #include "ringbuffer.h"
 
-uint16_t ringbuffer_create(RingBuffer_t* to_init, uint16_t length)
+uint16_t ringbuffer_init(RingBuffer_t* to_init, uint16_t length)
 {
+    uint8_t i;
+    if(to_init)
+    {    
+        if(length < RINGBUFFER_MAX_LEN)
+        {
+            for(i = 0; i < length; i++)
+            {
+                to_init->buffer[i] = 0;        
+            }
+            to_init->length = length;
+            to_init->pop_index = 0;
+            to_init->push_index = 0;
+            to_init->new_bytes = 0;
+        }
+        else
+        {
+            // desired length is above absolute limit!
+            return -1;
+        }
+
+    }
+    else
+    {
+        // ringbuffer to_init does not exist!
+        return -1;
+    }
+
 	return 0;
 }
 
-void ringbuffer_destroy(RingBuffer_t* to_destroy)
+uint8_t ringbuffer_destroy(RingBuffer_t* to_destroy)
 {
-	;
+    if(to_destroy)
+    {
+        to_destroy->buffer = NULL;
+        to_destroy->length = 0;
+        to_destroy->new_bytes = 0;
+        to_destroy->pop_index = 0;
+        to_destroy->push_index = 0;
+    }
+    else
+    {
+        return -1;
+    }
+
+    return 0;
 }
 
-uint16_t ringbuffer_read(RingBuffer_t *readfrom, uint8_t *readto, uint16_t amount)
+uint16_t ringbuffer_read(RingBuffer_t *readfrom, uint8_t *readto, uint8_t amount)
 {
+    uint8_t i;
+    uint8_t limit;
+    if(readto)
+    {
+        if(readfrom && readfrom->buffer)
+        {
+            if(amount <= readfrom->new_bytes)
+            {
+                limit = amount;
+            }
+            else
+            {
+                limit = readfrom->new_bytes;
+            }
+
+            for(i = 0; i < limit; i++)
+            {
+                readto[i] = readfrom->buffer[(readfrom->pop_index)++];
+            }
+        }
+    }
+    else
+    {
+        return -1;
+    }
 	return 0;
 }
 
--- ringbuffer.h
+++ ringbuffer.h
@@ -1,26 +1,26 @@
 #ifndef _ringbuffer_h
 #define _ringbuffer_h
 
-//#include <stdint.h>
+#include <stdint.h>
 
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
+#define RINGBUFFER_MAX_LEN 256
 
 typedef struct 
 {
-	uint8_t *buffer;
-	uint16_t length;
-	uint16_t start;
-	uint16_t end;
+	uint8_t*   buffer;
+    uint8_t    length;
+    uint8_t    new_bytes;
+	uint8_t    pop_index;
+	uint8_t    push_index;
 } RingBuffer_t;
 
-uint16_t ringbuffer_create(RingBuffer_t* to_init, uint16_t length);
+uint8_t ringbuffer_init(RingBuffer_t* to_init, uint8_t length);
 
 void ringbuffer_destroy(RingBuffer_t* to_destroy);
 
-uint16_t ringbuffer_read(RingBuffer_t *readfrom, uint8_t *readto, uint16_t amount);
+uint16_t ringbuffer_read(RingBuffer_t *readfrom, uint8_t *readto, uint8_t amount);
 
-uint16_t ringbuffer_write(RingBuffer_t *write_to, uint8_t *write_from, uint16_t amount);
+uint16_t ringbuffer_write(RingBuffer_t *write_to, uint8_t *write_from, uint8_t amount);
 
 uint16_t ringbuffer_empty(RingBuffer_t *to_check);
 
